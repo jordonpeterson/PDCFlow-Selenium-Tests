0e1842d432b1cf80672df64a8b028bb3cdf1a2f9
Jordon Peterson <jordonpropm@gmail.com>
2018-11-15 12:24:41 -0700

    Moved Environment to Python 2.7
    Wrote test that goes to Selenium's home site via a google search and verifies that it actually went there.

---
+++ google-selenium-test.py
@@ -0,0 +1,40 @@
+import sys
+import time
+
+from selenium import webdriver
+
+
+# I will assume we are using the FireFox driver.
+def test_google_selenium(browser):
+    "Test Google Selenium"
+
+    # Create an instance of WebDriver
+    if browser.lower() == 'firefox':
+        driver = webdriver.Firefox()
+    # Alert user if they are not using Firefox
+    else:
+        print('We need to use the Firefox Browser for this test')
+
+    # Navigate to google.com
+    driver.get('https://www.google.com/')
+
+    # Find the searchbox by name
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('selenium')
+
+    # Find the Google Search button by name and click it
+    google_search = driver.find_element_by_name('btnk')
+    google_search.click()
+
+    # Find the Selenium Link
+    selenium_site = driver.find_elements_by_link_text(
+        'Selenium - Web Browser Automation')
+    selenium_site.click()
+
+    # Test that we are at the correct URL.
+    assert driver.current_url == "https://www.seleniumhq.org/"
+    # Test that this URL is actually about Selenium
+    assert 'Selenium' in driver.title
+    print('Test Ran')
+    # Exit Browser
+    driver.quit()
