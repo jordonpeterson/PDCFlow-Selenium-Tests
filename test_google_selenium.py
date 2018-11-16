e98921a69243bff3c4909890b44facf77d28db52
Jordon Peterson <jordonpropm@gmail.com>
2018-11-15 17:19:10 -0700

    Updated file name to fit Python naming conventions

--- test_google_selenium.py
+++ test_google_selenium.py
@@ -51,7 +51,7 @@ def test_google_selenium(browser):
     # Check if all tests were passed
     assert total_tests == passed_tests
     print('all tests passed')
-    
+
     # print(f"{passed_tests} out of {total_tests} passed.")
     # The above line is python 3.6+ syntax that prints how many tests were passed.
     # It is not included in the Python 2.7 that I am using in this environment.
