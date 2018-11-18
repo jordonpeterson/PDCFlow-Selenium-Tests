0bb78f64665a37f049cf7692061dae37a03c600b
Jordon Peterson <jordonpropm@gmail.com>
2018-11-17 17:23:16 -0700

    Started new Facebook login test
    Not complete yet. Eventually I want to take a username and password as arguments and login to facebook. This will involve detecting if facebook needs a text message code and allowing the user to input that code into the program.

---
+++ facebook_login/test_facebook_login.py
@@ -0,0 +1,26 @@
+# imports
+from selenium import webdriver
+import time
+import sys
+
+
+def test_facebook_login(facebook_email='jordon.peterson@students.svu.edu', facebook_password='dontyouwishyouknew'):
+    "Test Facebook Login"
+
+    # Create driver and set up initial variables.
+    driver = webdriver.Firefox()
+
+    total_tests = 0
+    passed_tests = 0
+
+    # Navigate from Google to Facebook.com
+    driver.get('https://www.google.com/')
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('facebook')
+
+    google_search = driver.find_element_by_name('btnK')
+    print('We found the search button')
+    google_search.click()
+
+
+test_facebook_login()
