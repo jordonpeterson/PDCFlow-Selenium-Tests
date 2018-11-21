180c7683ab976d748ec8559be8a3a51127bd50f3
Jordon Peterson <jordonpropm@gmail.com>
2018-11-20 17:28:13 -0700

    Switched Google Search to hit_enter

--- page_objects/google_pom.py
+++ page_objects/google_pom.py
@@ -16,8 +16,20 @@ class Google():
         self.driver.find_element_by_name(
             self.google_searchbox_name).send_keys(search_term)
 
+    def hit_enter(self):
+        self.driver.find_element_by_name(
+        self.google_searchbox_name).send_keys(u'\ue007')
+
     def google_search(self):
         # The time.sleep method allows the page to refresh before we find the searchbox.
         # It would be better to use one of the await element methods here.
         time.sleep(2)
-        self.driver.find_element_by_name(self.google_searchbox_name).click()
+        print('google_search')
+        search_button = self.driver.find_element_by_name(
+            self.google_searchbox_name)
+        print('search button found')
+        time.sleep(2)
+        search_button.click()
+
+        print('button clicked')
+        time.sleep(5)
--- page_objects/test_google_pom.py
+++ page_objects/test_google_pom.py
@@ -3,30 +3,35 @@
 from selenium import webdriver
 import time
 from google_pom import Google
-import unittest
+import pytest
 
 
-class GoogleSearch():
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Firefox()
-        # Tells driver to search the DOM for up to 10 seconds before deciding
-        #  that an element isn't there. This gives the element time to load
-        cls.driver.implicitly_wait(10)
-        cls.driver.maximize_window()
+# class GoogleSearch(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.driver = webdriver.Firefox()
+#         # Tells driver to search the DOM for up to 10 seconds before deciding
+#         #  that an element isn't there. This gives the element time to load
+#         cls.driver.implicitly_wait(10)
+#         cls.driver.maximize_window()
 
-    def search_selenium(self):
-        driver = self.driver
-        google = Google(driver)
+def search_selenium(search_term):
+    driver = webdriver.Firefox()
+    google = Google(driver)
 
-        google.navigate_to_google()
+    google.navigate_to_google()
 
-        google.enter_search_term('Selenium')
+    google.enter_search_term(search_term)
 
-        google.google_search()
+    google.hit_enter()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.close()
-        cls.driver.quit()
-        print('Test Completed')
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.driver.close()
+#         cls.driver.quit()
+#         print('Test Completed')
+
+
+# if __name__ == "__main__":
+#     unittest.main()
+search_selenium('PDCFlow')
